6e6c8c9f03bcde0b442815855d90c8e3c90afdd3
Gia Hao <truonggiahao45@gmail.com>
2024-06-07 12:01:57 +0700

    new GET,PUSH,PATCH response

--- main.go
+++ main.go
@@ -1,25 +1,68 @@
 package main
 
-import ("github.com/gin-gonic/gin"
-		"net/http"
-		"fmt")
+import (
+	"errors"
+	"net/http"
 
-type todo struct{
-	ID 			string 		"json:'id'"
-	Item 		string		"json:'title'"
-	Completed 	bool 		"json:'completed'"
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
 }
-var todos =[]todo{
-	{ID: "1",Item:"Clean room",Completed:false},
-	{ID: "2",Item:"Read book",Completed: false},
-	{ID: "3",Item:"Record Video",Completed: true},
+
+var todos = []todo{
+	{ID: "1", Item: "Clean room", Completed: false},
+	{ID: "2", Item: "Read book", Completed: false},
+	{ID: "3", Item: "Record Video", Completed: true},
 }
-func getTodos(context *gin.Context){
-	context.IndentedJSON(http.StatusOK,todos)
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
 }
-func main(){
+func getTodoById(id string) (*todo, error) {
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("todo not found")
+}
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, error := getTodoById(id)
+	if error != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+	context.IndentedJSON(http.StatusOK, todo)
+}
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, error := getTodoById(id)
+	if error != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+	}
+	todo.Completed = !todo.Completed
+	context.IndentedJSON(http.StatusOK, todo)
+}
+func main() {
 	router := gin.Default()
-	router.GET("/todos",getTodos)
+	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
+	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
-	fmt.Print("1")
 }
